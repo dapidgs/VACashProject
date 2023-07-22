11bdce84ba062e38df8a0d44c8deaa1ad895ac6c
dapidgs <david.simanungkalit@binus.ac.id>
2023-07-23 00:31:41 +0700

    add first_button in activity_main.xml

--- app/src/main/java/com/example/vacashproject/MainActivity.java
+++ app/src/main/java/com/example/vacashproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.vacashproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button firstBtn = findViewById(R.id.first_btn);
+        firstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Selamat datang di android studio", Toast.LENGTH_LONG ).show();
+            }
+        });
     }
 }
