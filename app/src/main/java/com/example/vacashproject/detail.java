d9fefb9ed46e939cacd7beb9237d27c6dda2e3c8
dindadwins <dindadwinindita@gmail.com>
2023-07-29 09:33:57 +0700

    detail activity

--- app/src/main/java/com/example/vacashproject/detail.java
+++ app/src/main/java/com/example/vacashproject/detail.java
@@ -8,19 +8,80 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class detail extends AppCompatActivity {
 
     private Button button;
+    TextView plus, minus, total, price;
     EditText email, username;
+    private ProgressBar remainder;
+    TextView value;
+    int count = 0;
+    int totalPayment = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+//      submit button
         button = findViewById(R.id.buyBtn);
+
+//      stepper
+        plus = findViewById(R.id.increment);
+        minus = findViewById(R.id.decrement);
+
+//      input email & username
         email = findViewById(R.id.email);
         username = findViewById(R.id.username);
+
+//      diamonds information remainder
+        remainder = findViewById(R.id.remainder);
+        remainder.setMax(10000);
+        remainder.setProgress(7777);
+
+//      information needed for total payment
+        value = findViewById(R.id.quantity);
+        price = findViewById(R.id.itemPrice);
+        total = findViewById(R.id.totalPay);
+
+        price.setText("10000");
+        value.setText("0");
+        total.setText("IDR 0");
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price_check = price.getText().toString();
+                int price_now = Integer.parseInt(price_check);
+
+                count++;
+                totalPayment = count * price_now;
+                value.setText("" + count);
+                total.setText("IDR "+totalPayment);
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price_check = price.getText().toString();
+                int price_now = Integer.parseInt(price_check);
+
+                if(count <= 0) {
+                    count = 0;
+                }
+                else {
+                    count--;
+                }
+                totalPayment = count * price_now;
+                value.setText("" + count);
+                total.setText("IDR "+totalPayment);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,7 +97,11 @@ public class detail extends AppCompatActivity {
                     openDialog("Email must contain '.com'");
                 }
             }
+
+
         });
+
+
     }
     public void openDialog(String message){
         AlertDialog dialog = new AlertDialog.Builder(detail.this)
